9233c2fbfa00f9b5f93bb4b1b2516949dd3b18c9
FRJ74 <frankr74@gmail.com>
2024-08-07 16:07:29 -0600

    Get input from html elements

---
+++ script.js
@@ -0,0 +1,4 @@
+/* Get input from html elements */
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
