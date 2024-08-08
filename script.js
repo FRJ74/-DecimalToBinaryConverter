176a89404895824000dfe19c7a33bd2397c9a5f9
FRJ74 <frankr74@gmail.com>
2024-08-08 00:37:50 -0600

    Add a function & an eventhandler

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+
+/* Create a funtion to check the value in the number input element whenever the user clicks the Convert button. */
+const checkUserInput = () => {
+    console.log(numberInput.value);
+};
