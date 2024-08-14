30b00099f645865c3b7fc9640b40123c5802d90f
FRJ74 <frankr74@gmail.com>
2024-08-14 15:14:43 -0600

    Add codition to avoid an infinit loop and round down the result

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ const decimalToBinary = (input) => {
     const quotients = [];
     const remainders = [];
 
-    input = 0;
     while (input > 0) {
-  
+        const quotient = Math.floor(input / 2);
+        input = 0;
     }
 };
 
