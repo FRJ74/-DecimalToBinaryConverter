d2cd7714c0572ffff3a176fe4741b3f151191697
FRJ74 <frankr74@gmail.com>
2024-08-23 00:14:29 -0600

    rename the countdown() function to countDownAndUp()

--- script.js
+++ script.js
@@ -3,9 +3,11 @@
 const countDownAndUp=(number) => {
     console.log(number);
     if (number === 0) {
+        console.log("Reached base case");
         return;
       } else {
         countDownAndUp(number - 1);
+        console.log(number);
       }
 }; 
 
