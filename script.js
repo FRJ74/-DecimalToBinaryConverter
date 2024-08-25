3369c97074b4d7317da6a6574b99773a3ad63c11
FRJ74 <frankr74@gmail.com>
2024-08-25 12:11:44 -0600

    Add a 1500 millisecond delay before the text Camp is logged to the console.

--- script.js
+++ script.js
@@ -24,13 +24,35 @@ const decimalToBinary = (input) => {
        }
 };
 
+const showAnimation = () => {
+    setTimeout(() => {
+        console.log("free");
+      }, 500);
+       
+    setTimeout(() => {
+        console.log("Code");
+      }, 1000);
+      
+      setTimeout(() =>  {
+        console.log("Camp");
+      }, 1500);
+    };
+
+    
+
+
 /* Create a funtion to check the value in the number input element whenever the user clicks the Convert button. */
 const checkUserInput = () => {
-    if (!numberInput.value || isNaN(parseInt(numberInput.value)) || parseInt(numberInput.value) < 0) {
+
+    const inputInt = parseInt(numberInput.value);
+
+    if (!numberInput.value || 
+        isNaN(inputInt) ||
+        inputInt < 0) {
         alert("Please provide a decimal number greater than or equal to 0");
         return;
     }
-    result.textContent = decimalToBinary(parseInt(numberInput.value));
+    result.textContent = decimalToBinary(inputInt);
     numberInput.value = "";
 };
 
