9544097b24984e78e6c1e9e1e7720da355812cb5
FRJ74 <frankr74@gmail.com>
2024-08-30 16:06:47 -0600

    Set the textContent property of result equal to calling decimalToBinary() with 5 as an argument.

--- script.js
+++ script.js
@@ -1,79 +1,89 @@
-/* Create a function that will count down from a given number to zero using recursion */ 
-const countDownAndUp=(number) => {
-    console.log(number);
-    if (number === 0) {
-        console.log("Reached base case");
-        return;
-      } else {
-        countDownAndUp(number - 1);
-        console.log(number);
-      }
-}; 
-
-/* Declare variables & Get input from html elements */
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 const animationContainer = document.getElementById("animation-container");
 const animationData = [
-      {
-        inputVal: 5,
-        marginTop: 300,
-        addElDelay: 1000
-      },
-      {
-        inputVal: 2,
-        marginTop: -200,
-        addElDelay: 1500
-      },
-      {
-        inputVal: 1,
-        marginTop: -200,
-        addElDelay: 2000
-      },
+  {
+    inputVal: 5,
+    marginTop: 300,
+    addElDelay: 1000,
+    msg: 'decimalToBinary(5) returns "10" + 1 (5 % 2). Then it pops off the stack.',
+    showMsgDelay: 15000,
+    removeElDelay: 20000,
+  },
+  {
+    inputVal: 2,
+    marginTop: -200,
+    addElDelay: 1500,
+    msg: 'decimalToBinary(2) returns "1" + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.',
+    showMsgDelay: 10000,
+    removeElDelay: 15000,
+  },
+  {
+    inputVal: 1,
+    marginTop: -200,
+    addElDelay: 2000,
+    msg: "decimalToBinary(1) returns '1' (base case) and gives that value to the stack below. Then it pops off the stack.",
+    showMsgDelay: 5000,
+    removeElDelay: 10000,
+  }
 ];
 
-/* Convert user-provided input from decimal to binary*/
 const decimalToBinary = (input) => {
-    if (input === 0 || input === 1) {
-        return String(input);
-      } else {
-        return decimalToBinary(Math.floor(input / 2)) + (input % 2);
-       }
+  if (input === 0 || input === 1) {
+    return String(input);
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+  }
 };
 
-/* Animate output */
 const showAnimation = () => {
-    result.innerText = "Call Stack Animation";
-    animationData.forEach((obj) => {
-        setTimeout(() => {
-            animationContainer.innerHTML += 
-            `<p class="animation-frame" style="margin-top: ${obj.marginTop}px;" id="${obj.inputVal}">
-            decimalToBinary(${obj.inputVal})
-            </p>;`
-        }, obj.addElDelay );
-    });
-    };
+  result.innerText = "Call Stack Animation";
 
-/* Create a funtion to check the value in the number input element whenever the user clicks the Convert button. */
-const checkUserInput = () => {
+  animationData.forEach((obj) => {
+    setTimeout(() => {
+      animationContainer.innerHTML += `
+        <p id="${obj.inputVal}" style="margin-top: ${obj.marginTop}px;" class="animation-frame">
+          decimalToBinary(${obj.inputVal})
+        </p>
+      `;
+    }, obj.addElDelay);
 
-    const inputInt = parseInt(numberInput.value);
+    setTimeout(() => {
+      document.getElementById(obj.inputVal).textContent = obj.msg;
+    }, obj.showMsgDelay);
 
-    if (!numberInput.value || 
-        isNaN(inputInt) ||
-        inputInt < 0) {
-        alert("Please provide a decimal number greater than or equal to 0");
-        return;
-    }
-    result.textContent = decimalToBinary(inputInt);
-    numberInput.value = "";
+    setTimeout(() => {
+      document.getElementById(obj.inputVal).remove();
+    }, obj.removeElDelay);
+  });
+
+  setTimeout(() => {
+    result.textContent = decimalToBinary(5);
+  }, 20000);
 };
 
-/* Add event listener to the function */
+const checkUserInput = () => {
+  const inputInt = parseInt(numberInput.value);
+
+  if (!numberInput.value || isNaN(inputInt) || inputInt < 0) {
+    alert("Please provide a decimal number greater than or equal to 0");
+    return;
+  }
+
+  if (inputInt === 5) {
+    showAnimation();
+    return;
+  }
+
+  result.textContent = decimalToBinary(inputInt);
+  numberInput.value = "";
+};
+
 convertBtn.addEventListener("click", checkUserInput);
+
 numberInput.addEventListener("keydown", (e) => {
-    if (e.key === "Enter" ) {
-        checkUserInput();
-    };
-} );
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
